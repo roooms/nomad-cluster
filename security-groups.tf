58e052ede83c26f80067eae06bb58303a39228f0
Dan Brown <roooms@gmail.com>
2017-11-15 22:11:52 +0000

    add rule allowing all tcp within the nomad sg

--- security-groups.tf
+++ security-groups.tf
@@ -26,6 +26,14 @@ resource "aws_security_group" "nomad" {
   vpc_id      = "${var.vpc_id}"
 
   ingress {
+    description = "All TCP Internal"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
     description = "Nomad HTTP Internal"
     from_port   = 4646
     to_port     = 4646
